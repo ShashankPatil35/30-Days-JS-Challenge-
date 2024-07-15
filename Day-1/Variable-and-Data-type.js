6a70554ff8eb74e327822c71f3a469c5bf01d762
ShashankPatil35 <shashankmpatil3535@gmail.com>
2024-07-15 23:41:23 +0530

    Day-1

---
+++ Day-1/Variable-and-Data-type.js
@@ -0,0 +1,36 @@
+//Activity-1:Variable Declaration
+var num = 35;
+console.log(num);
+let aStr = "JS Challenge";
+console.log(aStr);
+
+//Activity-2:Constant Declaration
+const number = true;
+console.log(number);
+
+//Activity-3:Data Types
+let num1 = 10;
+let str = "This is a new String"
+let trick = false;
+let user =  {
+    username:"shashank",
+    password:"123456",
+    age:21,
+};
+let arr = ["user1","user2","user3","user4"];
+
+console.log(typeof num1)
+console.log(typeof str)
+console.log(typeof trick)
+console.log(typeof user)
+console.log(typeof arr)
+
+//Activity-4:Reassigning Variables
+let Variable = 9;
+console.log("Before reassigning: ",Variable);
+Variable = 12;
+console.log("Before reassigning: ",Variable);
+
+//Activity-5:Understanding Const
+// number = 8;
+//Throws error - TypeError: Assignment to constant variable.
